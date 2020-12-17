68a474558bae08dd0fc21d255e4cfc403b5700f7
Kevin Killman <kevinkillman@gmail.com>
2020-12-16 21:40:57 -0500

    fix

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ db = SQLAlchemy(app)
 Pet = create_classes(db)
 
 # create route that renders index.html template
-@app.route("/hi")
+@app.route("/")
 def home():
     return render_template("index.html")
 
